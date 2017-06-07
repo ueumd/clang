59608284d367a0e8a7095b6ca7cfdd06b549b2d3
hsd <51134642@qq.com>
2017-06-07 20:32:16 +0800

    c++

--- cplusplusclass/cplusplusclass/d3-2-深拷贝和浅拷贝.cpp
+++ cplusplusclass/cplusplusclass/d3-2-深拷贝和浅拷贝.cpp
@@ -1,6 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "iostream"
-
 using namespace std;
 
 class Name
@@ -8,22 +7,25 @@ class Name
 public :
 	Name(const char *myp)
 	{
-		int len = strlen(myp);
+		len = strlen(myp);
 		p = (char *)malloc(len + 1);
 		strcpy(p, myp);
 	}
-	Name(const Name *myp)
+	//解决方案 ：手工编写拷贝构造函数 使用深拷贝
+	Name(const Name& obj)
 	{
-		int len = strlen(myp);
+		len = obj.len;
 		p = (char *)malloc(len + 1);
-		strcpy(p, myp);
+		strcpy(p, obj.p);
 	}
+
 	~Name()
 	{
 		if (p != NULL)
 		{
 			free(p);
 			p = NULL;
+			len = 0;
 		}
 	}
 private:
@@ -31,10 +33,14 @@ private:
 	int len;
 };
 
+//
 void testName()
 {
 	Name obj1("abcefg");
-	Name obj2 = obj1;
+	Name obj2 = obj1;//默认的copy构造函数  c++编译器提供 对象析构的时候 出问题
+
+	Name obj3("aaa");
+	obj3 = obj1; // = 等号操作 同样问题 浅拷贝  obj1被析构时 obj3里指向垃圾空间 变成野指针
 }
 
 void main()
