fb4201302de601e8261b52b852f3f45d5f2bb491
daysun <51134642@qq.com>
2017-11-22 10:06:02 +0800

    add

---
+++ yicheng/3位操作与二进制/内存.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+
+	int num = 1;
+	printf("%d", sizeof(num));
+	printf("\n%p", &num);
+	/*
+	32位 占4个字节 1列 16进制显示
+	0x0039FD78  01  .
+	0x0039FD79  00  .
+	0x0039FD7A  00  .
+	0x0039FD7B  00  .
+
+	0000
+	0000
+	0000
+	0001
+	*/
+
+	int x = 0x12345678; //16进制数据
+	printf("%d", sizeof(x));
+	printf("\n%p", &x);
+	//低位在低字节 高位在高字节
+	/*
+	0x0033F9A8  78  x
+	0x0033F9A9  56  V
+	0x0033F9AA  34  4
+	0x0033F9AB  12  .
+	*/
+	system("pause");
+	return 0;
+}
